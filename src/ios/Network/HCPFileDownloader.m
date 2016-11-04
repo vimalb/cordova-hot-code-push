f02fc7629afa9424aee343cf05e2554647d6f5d5
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2016-11-04 13:56:34 +0100

    iOS. Added file validation after loading it.

--- src/ios/Network/HCPFileDownloader.m
+++ src/ios/Network/HCPFileDownloader.m
@@ -66,28 +66,23 @@ static NSUInteger const TIMEOUT = 300;
 - (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(NSError *)error {
     if (error && _complitionHandler) {
         _complitionHandler(error);
-    }
-}
-
-- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
-    if (error) {
-        [_session invalidateAndCancel];
-        _complitionHandler(error);
-        return;
+        _session = nil;
     }
 }
 
 - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
     NSError *error = nil;
     if (![self moveLoadedFile:location forFile:_filesList[_downloadCounter] toFolder:_folderURL error:&error]) {
-        _complitionHandler(error);
         [_session invalidateAndCancel];
+        _session = nil;
+        _complitionHandler(error);
         return;
     }
     
     _downloadCounter++;
     if (_downloadCounter >= _filesList.count) {
         [_session finishTasksAndInvalidate];
+        _session = nil;
         _complitionHandler(nil);
         return;
     }
@@ -113,16 +108,13 @@ static NSUInteger const TIMEOUT = 300;
  *
  *  @return <code>YES</code> if data is corrupted; <code>NO</code> if data is valid
  */
-- (BOOL)isDataCorrupted:(NSData *)data forFile:(HCPManifestFile *)file error:(NSError **)error {
-    *error = nil;
-    NSString *fileChecksum = file.md5Hash;
-    NSString *dataHash = [data hashWithAlgorithm:[fileChecksum getHashAlgorithm]];
-    if ([dataHash isEqualToString:fileChecksum]) {
+- (BOOL)isFileCorrupted:(NSURL *)file checksum:(NSString *)checksum {
+    NSString *dataHash = [[NSData dataWithContentsOfURL:file] hashWithAlgorithm:[checksum getHashAlgorithm]];
+    if ([dataHash isEqualToString:checksum]) {
         return NO;
     }
     
-    NSString *errorMsg = [NSString stringWithFormat:@"Hash %@ of the file %@ doesn't match the checksum %@", dataHash, file.name, fileChecksum];
-    *error = [NSError errorWithCode:kHCPFailedToDownloadUpdateFilesErrorCode description:errorMsg];
+    NSLog(@"Hash %@ doesn't match the checksum %@", dataHash, checksum);
     
     return YES;
 }
@@ -139,10 +131,12 @@ static NSUInteger const TIMEOUT = 300;
  */
 
 - (BOOL)moveLoadedFile:(NSURL *)loadedFile forFile:(HCPManifestFile *)file toFolder:(NSURL *)folderURL error:(NSError **)error {
-//    if ([self isDataCorrupted:data forFile:file error:error]) {
-//        return NO;
-//    }
-//    
+    if ([self isFileCorrupted:loadedFile checksum:file.md5Hash]) {
+        NSString *errorMsg = [NSString stringWithFormat:@"File %@ is corrupted", file.name];
+        *error = [NSError errorWithCode:kHCPFailedToDownloadUpdateFilesErrorCode description:errorMsg];
+        return NO;
+    }
+    
     NSURL *filePath = [folderURL URLByAppendingPathComponent:file.name];
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
@@ -158,8 +152,6 @@ static NSUInteger const TIMEOUT = 300;
                                  error:nil];
     
     // write data
-    //return [data writeToURL:filePath options:kNilOptions error:error];
-    
     return [fileManager moveItemAtURL:loadedFile toURL:filePath error: error];
 }
 
